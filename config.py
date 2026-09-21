94bca93436269b4ad132d7a76449f25bf870b59e
尽余欢 <3023975933@qq.com>
2026-09-21 23:07:25 +0800

    更新 config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 # 优先从环境变量读取 Cookie（部署到云服务器时更安全）
 # 本地测试时可以把下面的默认值改成你的完整 Cookie
-COOKIE = os.environ.get("ELECTRIC_COOKIE", "shiroJID=9d6e833f-7568-4fe9-9455-40b91fc4c6ff")
+COOKIE = os.environ.get("ELECTRIC_COOKIE", "shiroJID=580988f4-dbeb-4e29-8247-acf11731c550")
 
 # 宿舍参数（已根据你的抓包填写）
 AREA_ID = "2408569690932133889"
